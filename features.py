acb0ee289433d2cc1617f5e540331d9b8c649517
AnastasiaVepreva <145943513+AnastasiaVepreva@users.noreply.github.com>
2024-04-03 20:09:53 +0300

    Update features.py

--- features.py
+++ features.py
@@ -1,17 +1,8 @@
 import pandas as pd
 import numpy as np
-from rdkit import Chem
 
-df = pd.read_csv('vepreva_dataset.csv')
+df = pd.read_csv('qm9.csv')
 
-smiles = df['smiles'].to_list()
+random = df.sample(n = 20000, random_state = 2)
 
-mols = [Chem.MolFromSmiles(i) for i in smiles]
-MorganFP = []
-header = ['bit' + str(i) for i in range(2048)]
-for i in range(len(mols)):
-    fps = list(Chem.RDKFingerprint(mols[i]).ToBitString())
-    MorganFP.append(fps)
-df2 = pd.DataFrame(MorganFP,columns=header)
-df2.insert(loc=0, column='smiles', value=smiles)
-print(df2) 
+random.to_csv('vepreva_dataset.csv', index=True)
